a3a33472306fb8d6d978883cdc2a80874cc66e5e
xgqfrms <xgqfrms@outlook.com>
2023-04-27 03:40:13 +0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Python UTF-8
+# coding: utf8
+
+echo "hello world! 🌏 "
+
+echo "hello shell script! 🚀 "
+
+
+<<EOF
+"""
+
+#
+$ bash ./hello.sh
+
+# 或
+$ chmod +x ./hello.sh
+$ ./hello.sh
+
+"""
+EOF
